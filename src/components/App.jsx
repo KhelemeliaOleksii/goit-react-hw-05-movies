04bd63a34e4ec1baaf7a0c001df3a81e653496d1
KhelemeliaOleksii <xvdm@ukr.net>
2022-06-02 07:39:07 +0300

    add lazy loading of pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,36 @@
 import { Navigate, Outlet, Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+
 import AppBar from "./AppBar";
 import Container from "./Container";
-import HomePage from "pages/HomePage";
-import MoviesPage from "pages/MoviesPage";
-import MovieDetailsPage from "pages/MovieDetailsPage";
-import Cast from "./Cast";
-import Reviews from "./Reviews";
+import Loader from "./Loader";
+
+// для webpackChunkName є навіть різниця у записі:
+// /*webpackChunkName: "home-page"  */ - згенерує webpack Chunk Name як "home-page" 
+// /*webpackChunkName : "home-page" */ - згенерує ім'я автоматично
+const HomePage = lazy(() => import('pages/HomePage' /*webpackChunkName: "home-page"  */));
+const MoviesPage = lazy(() => import('pages/MoviesPage' /* webpackChunkName: "movies-page" */));
+const MovieDetailsPage = lazy(() => import('pages/MovieDetailsPage' /*webpackChunkName: "movie-details-page"*/));
+const Cast = lazy(() => import('./Cast' /* webpackChunkName: "cast" */));
+const Reviews = lazy(() => import('./Reviews'/* webpackChunkName: "reviews" */));
 
 export const App = () => {
   return (
     <Container>
       <AppBar />
       <Outlet />
-      <Routes >
-        <Route path='/' element={<HomePage />} />
-        <Route path='/movies' element={<MoviesPage />} />
-        <Route path='/movies/:movieId' element={<MovieDetailsPage />} >
-          <Route path='cast' element={<Cast />} />
-          <Route path='reviews' element={<Reviews />} />
-        </Route>
-        <Route path='*' element={<Navigate to='/' replace />} />
-      </Routes>
-    </Container>
+      <Suspense fallback={<Loader />}>
+        <Routes >
+          <Route path='/' element={<HomePage />} />
+          <Route path='/movies' element={<MoviesPage />} />
+          <Route path='/movies/:movieId' element={<MovieDetailsPage />} >
+            <Route path='cast' element={<Cast />} />
+            <Route path='reviews' element={<Reviews />} />
+          </Route>
+          <Route path='*' element={<Navigate to='/' replace />} />
+        </Routes>
+      </Suspense>
+    </Container >
   );
 };
 
